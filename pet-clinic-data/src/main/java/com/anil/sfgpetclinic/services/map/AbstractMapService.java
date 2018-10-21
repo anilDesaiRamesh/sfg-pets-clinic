9eef8edeff74765cc54b68f4286d181ad41ed025
Anil_Ramesh <anil.hhh.desai@gmail.com>
2018-10-21 18:41:51 +0100

    Updated Map Impl to manage setting of ID property. Closes #23

--- pet-clinic-data/src/main/java/com/anil/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/anil/sfgpetclinic/services/map/AbstractMapService.java
@@ -3,18 +3,22 @@
  */
 package com.anil.sfgpetclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
+
+import com.anil.sfgpetclinic.model.BaseEntity;
 
 /**
  * @author Anil_Ramesh
  *
  */
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-	protected Map<ID, T> map = new HashMap<>();
+	protected Map<Long, T> map = new HashMap<>();
 
 	Set<T> findAll() {
 		return new HashSet(map.values());
@@ -24,8 +28,15 @@ public abstract class AbstractMapService<T, ID> {
 		return map.get(id);
 	}
 
-	T save(ID id, T object) {
-		map.put(id, object);
+	T save(T object) {
+		if (object != null) {
+			if (object.getId() == null) {
+				object.setId(getNextId());
+			}
+		} else
+			throw new RuntimeException("Null Object passed");
+
+		map.put(object.getId(), object);
 		return object;
 	}
 
@@ -35,6 +46,19 @@ public abstract class AbstractMapService<T, ID> {
 
 	void deleteById(ID id) {
 		map.remove(id);
+	}
+
+	private Long getNextId() {
+
+		Long nextId = null;
+
+		try {
+			nextId = Collections.max(map.keySet()) + 1;
+		} catch (NoSuchElementException e) {
+			nextId = 1L;
+
+		}
+		return nextId;
 	}
 
 }
