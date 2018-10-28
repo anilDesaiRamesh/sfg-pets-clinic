09a117ed03a19c5c259bcb9512885bd41efb7c58
Anil_Ramesh <anil.hhh.desai@gmail.com>
2018-10-28 12:58:57 +0000

    Created Vet Specialty and associated that with Vet entity. Closes #27

---
+++ pet-clinic-data/src/main/java/com/anil/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.anil.sfgpetclinic.model;
+
+/**
+ * @author Anil_Ramesh
+ *
+ */
+public class Specialty extends BaseEntity {
+
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
--- pet-clinic-data/src/main/java/com/anil/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/anil/sfgpetclinic/model/Vet.java
@@ -3,10 +3,22 @@
  */
 package com.anil.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author Anil_Ramesh
  *
  */
 public class Vet extends Person {
+
+	private Set<Specialty> specialty;
+
+	public Set<Specialty> getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(Set<Specialty> specialty) {
+		this.specialty = specialty;
+	}
 
 }
